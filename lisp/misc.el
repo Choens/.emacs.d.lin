010a9f43e1139c580cb5207f579836d743beea1e
Andy Choens <andy.choens@gmail.com>
2014-07-14 23:51:08 -0400

    Updates to lisp folder
    A collection of changes to the lisp folder.

---
+++ lisp/misc.el
@@ -0,0 +1,146 @@
+;; #############################################################################
+;; misc.el
+;;
+;; Collection of miscellaneous lisp I have collected over the years.
+;;
+;; Note:
+;; Where possible, I document the original source of the function, but the
+;; function here may or may not be the same as the original source function(s).
+;;
+;; - Open Functions
+;; - Highlight Functions
+;; - toggle-fullscreen
+;; - Text Manipulation
+;; #############################################################################
+
+
+;; =============================================================================
+;; -- Open Functions --
+;; =============================================================================
+
+;; -- User-Specific Files --
+(defun open-passwords ()
+  (interactive)
+  (find-file "~/Documents/Passwords.org"))
+
+(defun open-andy ()
+  (interactive)
+  (find-file "/media/andy/Sync/Sync/Andy-TODO.org"))
+
+(defun open-buy ()
+  (interactive)
+  (find-file "/media/andy/Sync/Sync/Buy-TODO.org"))
+
+(defun open-work ()
+  (interactive)
+  (find-file "/media/andy/Sync/Sync/Work-TODO.org"))
+
+(defun open-sync-drive ()
+  (interactive)
+  (find-file "/media/andy/Sync/"))
+
+(defun open-sync-local ()
+  (interactive)
+  (find-file "~/sync/"))
+
+(defun open-git ()
+  (interactive)
+  (find-file "~/Git/"))
+
+;; -- Emacs-Specific Files --
+(defun open-init ()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(defun open-lisp ()
+  (interactive)
+  (find-file "~/.emacs.d/lisp/"))
+
+(defun open-sql-conn ()
+  (interactive)
+  (find-file "~/config/sql-connections.el"))
+
+(defun open-templates-file ()
+  (interactive)
+  (find-file "/media/andy/Sync/Sync/analytic_templates/file_templates"))
+ 
+(defun open-templates-proj ()
+  (interactive)
+  (find-file "/media/andy/Sync/Sync/analytic_templates/project_templates"))
+ 
+(defun open-templates-snip ()
+  (interactive)
+  (find-file "/media/andy/Sync/Sync/analytic_templates/snippets"))
+
+;; -- Installation Files / Notes --
+(defun open-desktop ()
+  (interactive)
+  (find-file "~/config/install/Optimus/Install-Desktop.sh"))
+
+(defun open-programming ()
+  (interactive)
+  (find-file "~/config/install/Optimus/Install-Programming.sh"))
+
+;; =============================================================================
+;; -- Highlight Functions --
+;; =============================================================================
+
+(defun hl-todo ()
+	"Highlight all instances of the phrase 'TODO - '"
+	(interactive)
+	(highlight-phrase "TODO - "))
+
+(defun hl-fixme ()
+	"Highlight all instanaces of the phrase 'FIXME - '"
+	(interactive)
+	(highlight-phrase "FIXME - "))
+
+(defun hl-note ()
+	"Highlight all instanaces of the phrase 'NOTE - '"
+	(interactive)
+	(highlight-phrase "NOTE - "))
+
+
+;; ============================================================================= 
+;; -- toggle-fullscreen --
+;; Source: http://www.emacswiki.org/emacs/FullScreen#toc17
+;; =============================================================================
+(defun toggle-fullscreen ()
+  "Toggles full screen on X11"
+  (interactive)
+  (when (eq window-system 'x)
+    (set-frame-parameter
+     nil 'fullscreen
+     (when (not (frame-parameter nil 'fullscreen)) 'fullboth))))
+
+
+;; =============================================================================
+;; -- Text Manipulation --
+;; =============================================================================
+
+;; -----------------------------------------------------------------------------
+;; Clears the current buffer.
+;; -----------------------------------------------------------------------------
+(defun clear-buffer ()
+  (interactive)
+  (mark-whole-buffer)
+  (delete-region (region-beginning) (region-end)))
+
+;; -----------------------------------------------------------------------------
+;; Use M-w and C-w on entire line when a
+;; region is not selected.
+;; Source: http://www.emacswiki.org/emacs/WholeLineOrRegion
+;; -----------------------------------------------------------------------------
+(put 'kill-ring-save 'interactive-form
+     '(interactive
+       (if (use-region-p)
+           (list (region-beginning) (region-end))
+         (list (line-beginning-position) (line-beginning-position 2)))))
+
+(put 'kill-region 'interactive-form      
+     '(interactive
+       (if (use-region-p)
+           (list (region-beginning) (region-end))
+         (list (line-beginning-position) (line-beginning-position 2)))))
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
---
+++ lisp/mode-specific-config.el
@@ -0,0 +1,271 @@
+;; #############################################################################
+;; -- Mode Specific Config --
+;;
+;;   - Dired
+;;   - EPA Mode
+;;   - ESS Mode
+;;   - Ido Mode
+;;   - Magit
+;;   - Markdown
+;;   - Org Mode
+;;   - Polymode
+;;   - Python
+;;   - SQL Mode
+;;   - TRAMP Mode
+;;   - Web Development
+;; #############################################################################
+
+;; =============================================================================
+;; -- Dired --
+;;
+;; - Makes sizes human-readable, sorts version numbers
+;; - Orders dotfiles and capital-letters first.
+;; - Suggests dired targets
+;; =============================================================================
+(setq-default dired-listing-switches "-alhv")
+(setq dired-recursive-copies 'always)
+(setq dired-dwim-target t)
+
+
+;; =============================================================================
+;; -- EPA Mode --
+;;
+;; - Enables Easy PG (GNU PG interface for Emacs)
+;; =============================================================================
+(autoload 'epa-file "epa-file.elc")
+
+
+;; =============================================================================
+;; -- ESS Mode --
+;;
+;; ESS is the layer connecting Emacs to R, SAS, etc.
+;; - 
+;; =============================================================================
+(autoload 'ess-R-data-view "ess-R-data-view.el")
+(setq-default ess-indent-offset 4)
+(setq comint-scroll-to-bottom-on-input t)
+(setq comint-scroll-to-bottom-on-output t)
+(setq comint-move-point-for-output t)
+(setq ess-use-auto-complete t)
+(setq ess-help-own-frame nil)
+;(ess-toggle-underscore nil)
+;(setq ess-S-assign-key (kbd "C-="))
+;(setq ess-toggle-S-assign-key t)
+
+
+;; =============================================================================
+;; -- Ido Mode --
+;; 
+;; - Enables IDO Mode
+;; - Enables flexible matching
+;; =============================================================================
+(ido-mode t)
+(setq ido-enable-flex-matching t)
+
+
+;; =============================================================================
+;; -- Magit Mode
+;; =============================================================================
+(require 'magit)
+
+
+;; =============================================================================
+;; -- Markdown Mode
+;; =============================================================================
+(autoload 'markdown-mode "markdown-mode.el" t)
+
+(setq auto-mode-alist
+      (append '(
+                ("\\.text" . markdown-mode)
+                ("\\.md" . markdown-mode)
+                ("\\.Rmd" . markdown-mode)
+                )
+              auto-mode-alist))
+
+
+;; =============================================================================
+;; -- Org Mode --
+;; =============================================================================
+
+(setq org-agenda-files (list "~/Documents/ToDo.org"))
+(setq org-default-notes-file "~/Documents/Notes.org")
+
+(setq org-src-fontify-natively t)
+(add-hook 'text-mode-hook '(lambda () (auto-fill-mode 1)))
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+;; -- Babelfish -- 
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '(
+   (ditaa . t)
+   (emacs-lisp . t)
+   (latex . t)
+   (python . t)
+   (R . t)
+   (sh . t)
+   (sql . t)
+   (sqlite . t)
+   ))
+(setq org-confirm-babel-evaluate nil)
+
+;; =============================================================================
+;; -- Polymode --
+;; =============================================================================
+(require 'polymode-common)
+(require 'polymode-classes)
+(require 'polymode-methods)
+(require 'polymode-export)
+(require 'polymode-weave)
+(require 'poly-R)
+(require 'poly-markdown)
+
+(add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.Snw" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rnw" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rmd" . poly-markdown+r-mode))
+
+;; =============================================================================
+;; -- Python Mode --
+;; =============================================================================
+
+;; -- IPython --
+(setq ipython-command "/usr/bin/ipython3")
+(setq-default py-python-command-args '("-pylab" "-matplotlib"))
+(autoload 'ipython "ipython.el" t)
+
+(setq
+ python-shell-interpreter "ipython3"
+ python-shell-interpreter-args ""
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
+
+;; -- Basic Python Config --
+(setq python-shell-interpreter "ipython3" )
+;;(setq-default py-indent-offset 4)
+
+;; EIN
+;; Shell ipython3 notebook
+;; Emacs M-x ein:notebooklist-open
+(autoload 'ein "ein.el" t)
+(setq ein:use-auto-complete t)
+
+
+;; =============================================================================
+;; -- SQL Mode --
+;; =============================================================================
+(setq-default sql-indent-offset 2)
+(load-file "~/config/sql-connections.el" ) 
+
+
+(setq auto-mode-alist
+      (append '(("\\.sql$" . sql-mode)
+                ("\\.tbl$" . sql-mode)
+                ("\\.sp$"  . sql-mode))
+              auto-mode-alist))
+
+(set 'sql-sqlite-program "sqlite3")
+(set 'sql-sybase-program "sqsh")
+
+;; Runs SQL commands asynchronously, improves usability for big stuff.
+(set 'sql-preferred-evaluation-method "background")
+
+;; Save SQL History in product-specific files.
+;; Source: http://www.emacswiki.org/emacs/SqlMode
+(defun my-sql-save-history-hook ()
+  (let ((lval 'sql-input-ring-file-name)
+        (rval 'sql-product))
+    (if (symbol-value rval)
+        (let ((filename 
+               (concat "~/.emacs.d/sql/"
+                       (symbol-name (symbol-value rval))
+                       "-history.sql")))
+          (set (make-local-variable lval) filename))
+      (error
+       (format "SQL history will not be saved because %s is nil"
+               (symbol-name rval))))))
+ 
+(add-hook 'sql-interactive-mode-hook 'my-sql-save-history-hook)
+
+
+;; Make SQL Returns look nicer (lines things up correctly).
+;; Source: http://www.emacswiki.org/emacs/SqlMode
+(defvar sql-last-prompt-pos 1
+  "position of last prompt when added recording started")
+(make-variable-buffer-local 'sql-last-prompt-pos)
+(put 'sql-last-prompt-pos 'permanent-local t)
+
+(defun sql-add-newline-first (output)
+  "Add newline to beginning of OUTPUT for `comint-preoutput-filter-functions'
+    This fixes up the display of queries sent to the inferior buffer
+    programatically."
+  (let ((begin-of-prompt
+         (or (and comint-last-prompt-overlay
+                  ;; sometimes this overlay is not on prompt
+                  (save-excursion
+                    (goto-char (overlay-start comint-last-prompt-overlay))
+                    (looking-at-p comint-prompt-regexp)
+                    (point)))
+             1)))
+    (if (> begin-of-prompt sql-last-prompt-pos)
+        (progn
+          (setq sql-last-prompt-pos begin-of-prompt)
+          (concat "\n" output))
+      output)))
+
+(defun sqli-add-hooks ()
+  "Add hooks to `sql-interactive-mode-hook'."
+  (add-hook 'comint-preoutput-filter-functions
+            'sql-add-newline-first))
+
+(add-hook 'sql-interactive-mode-hook 'sqli-add-hooks)
+
+
+;; =============================================================================
+;; -- TRAMP Mode --
+;; =============================================================================
+;(setq tramp-default-method "ssh")
+
+
+;; =============================================================================
+;; -- Web Development --
+;; =============================================================================
+(autoload 'html-helper-mode "html-helper-mode" "Yay HTML" t)
+(setq auto-mode-alist
+      (append '(
+                ("\\.html$" . html-helper-mode) 
+                ("\\.asp$" . html-helper-mode)  
+                ("\\.phtml$" . html-helper-mode)
+                ("\\.php$" . php-mode)
+                ("\\.PHP$" . php-mode)
+                )
+              auto-mode-alist))
+
+
+
+;; -- PHP --
+;; (autoload 'php-mode "php-mode.el" t)
+
+;; (setq auto-mode-alist
+;;       (append '(("\\.php$" . php-mode)
+;;                 ("\\.PHP$" . php-mode))
+;;               auto-mode-alist))
+
+;; To use abbrev-mode, add lines like this:
+;;   (add-hook 'php-mode-hook
+;;     '(lambda () (define-abbrev php-mode-abbrev-table "ex" "extends")))
+
+;; To make php-mode compatible with html-mode, see http://php-mode.sf.net
+
+;; Many options available under Help:Customize
+;; Options specific to php-mode are in
+;;  Programming/Languages/Php
+;; Since it inherits much functionality from c-mode, look there too
+;;  Programming/Languages/C
