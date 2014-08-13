1bf1dffbaf35eb6c455d991ef0906f1cd5ce1097
Andy Choens <andy.choens@gmail.com>
2014-08-13 13:54:53 -0400

    Matches the TODO Keywords between Linux and Windows.

--- lisp/mode-specific-config.el
+++ lisp/mode-specific-config.el
@@ -111,7 +111,7 @@
                                "~/Git/Notes/uag-1st-friday-website.org"
                               )))
 (setq org-todo-keywords
-           '((sequence "TODO(t)" "|" "DONE(d!)")
+           '((sequence "TODO(t@)" "IN PROGRESS(i@)" "|" "DONE(d@/!)")
              (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f!)")
              (sequence "|" "CANCELED(c@/!)")
              (sequence "|" "WAITING(w@/!)")
