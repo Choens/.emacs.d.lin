e83dc815b768d01a94966ff7d21fb27b9f8a1579
Andy Choens <andy.choens@gmail.com>
2014-08-31 16:18:15 -0400

    Adds php files to web-mode.

--- lisp/mode-specific-config.el
+++ lisp/mode-specific-config.el
@@ -289,6 +289,7 @@
 
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode)) 
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode)) 
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode)) 
 (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode)) 
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode)) 
