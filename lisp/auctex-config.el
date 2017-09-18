ff95bd96fa24826a87fe1e592c7dd41be08929b8
Andy Choens <andy.choens@gmail.com>
2017-09-17 22:07:48 -0400

    Initial Commit

---
+++ lisp/auctex-config.el
@@ -0,0 +1,17 @@
+;; #############################################################################
+;; -- auctex --
+;;
+;; Edit LaTeX in Emacs!
+;; https://www.emacswiki.org/emacs/AUCTeX
+;;
+;; #############################################################################
+
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+(setq TeX-PDF-mode t)
