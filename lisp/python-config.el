6f3aad14af04394e1bbe0906fd8c7737e8d5529a
Andy Choens <andy.choens@gmail.com>
2018-04-23 13:57:22 -0400

    Improves emacs setup for python.
    - Is this everything I've ever wanted - no.
    - Is it better? Yes. It is.
    
    Changes to be committed:
        modified:   lisp/python-config.el

--- lisp/python-config.el
+++ lisp/python-config.el
@@ -2,13 +2,20 @@
 ;; -- python --
 ;; #############################################################################
 
-(require 'python)
-(when (executable-find "ipython3")
-  (setq
-   python-shell-interpreter "ipython3"
-   python-shell-interpreter-args "--simple-prompt -i"
-   ;;python-shell-interpreter-args "--pylab=qt --matplotlib=qt"
-   ))
+;; If you want to use regular python.
+;; (setq python-shell-interpreter "python"
+;;       python-shell-interpreter-args "-i")
 
-;(require 'python-cell)
-;(add-hook 'python-mode-hook #'python-cell-mode 1)
+
+;; If you want to use ipython.
+(setq python-shell-interpreter "ipython3"
+      python-shell-interpreter-args "-i --simple-prompt")
+
+
+;; If you want to use jupyter.
+;; (setq python-shell-interpreter "jupyter"
+;;       python-shell-interpreter-args "console --simple-prompt"
+;;       python-shell-prompt-detect-failure-warning nil)
+;; (add-to-list 'python-shell-completion-native-disabled-interpreters
+;;              "jupyter")
+
