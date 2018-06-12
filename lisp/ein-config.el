7d40f0cdaff46b1ec54b9f3213dae5b8cf5ba3a7
Andy Choens <andy.choens@acuitashealth.com>
2018-06-12 11:39:27 -0400

    Local Updates

--- lisp/ein-config.el
+++ lisp/ein-config.el
@@ -3,12 +3,13 @@
 ;; #############################################################################
 
 ;; use autocompletion, but don't start to autocomplete after a dot
-(setq ein:complete-on-dot -1)
-(setq ein:use-auto-complete 1)
-(setq ein:query-timeout 1000)
-(require 'ein)
+;; (setq ein:complete-on-dot -1)
+;; (setq ein:use-auto-complete 1)
+;; (setq ein:query-timeout 1000)
+;; (setq ein:jupyter-default-server-command 'jupyter_app)
+;; (require 'ein)
 
-(setq ein:use-auto-complete t)
+;; (setq ein:use-auto-complete t)
 
 ; shortcut function to load notebooklist
 ;(defun load-ein () 
