041f2926ef06524e97ea400df85296a00494b11d
Andy Choens <andy.choens@acuitashealth.com>
2018-12-24 00:38:55 -0500

    Minor cleanup to init.el
    - Removed the list of packages to install and other cruft added by Emacs.
    
    Changes to be committed:
      modified:   init.el

--- init.el
+++ init.el
@@ -99,13 +99,4 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default)))
- '(package-selected-packages
-   (quote
-    (anaconda-mode counsel csv-mode ein elpy emmet-mode eval-in-repl flycheck flycheck magit markdown-mode material-theme org-gnome org-plus-contrib ox-reveal persp-projectile poly-R poly-markdown poly-noweb poly-org polymode py-autopep8 solarized-theme swiper web-mode yaml-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+    ("732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default))))
