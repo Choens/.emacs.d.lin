1db3357d8745349d8fceceb3785b0ee98bc86817
Andy Choens <andy.choens@acuitashealth.com>
2018-12-29 21:16:21 -0500

    Minor alterations to clipboard, copying, etc. settings.
    Changes to be committed:
      modified:   lisp/edit.el

--- lisp/edit.el
+++ lisp/edit.el
@@ -20,14 +20,14 @@
 (put 'narrow-to-page 'disabled nil)     ; Enable Narrow to page.
 (put 'narrow-to-region 'disabled nil)	; Enable Narrow to region.
 (put 'downcase-region 'disabled nil)
-;;(setq select-enable-clipboard 1)        ; When in a terminal, use X-Clipboard
-;;(setq select-enable-primary 1)          ; When in a terminal, use Primary
+;;(setq select-enable-clipboard 1)      ; When in a terminal, use X-Clipboard
+;;(setq select-enable-primary 1)        ; When in a terminal, use Primary
 
 
 
 ;; -- Backup --
-(setq  backup-by-copying t )            ; Enables Emacs backups
-(setq   backup-directory-alist          ; Sets the backup directory
+(setq backup-by-copying t )             ; Enables Emacs backups
+(setq backup-directory-alist            ; Sets the backup directory
     '(("." . "~/.emacs.d/backups")) )     
 (setq delete-old-versions t )           ; Delete old versions
 (setq kept-new-versions 5 )             ; Keep no more than 5 new versions
