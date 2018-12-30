6a7df6f90d7aaf66df595c4a193d11e4ffaf6867
Andy Choens <andy.choens@acuitashealth.com>
2018-12-29 21:19:01 -0500

    Adds ob-ipython
    Changes to be committed:
      modified:   lisp/orgmode-config.el

--- lisp/orgmode-config.el
+++ lisp/orgmode-config.el
@@ -3,61 +3,65 @@
 ;; Reference:
 ;; - http://orgmode.org/
 
+(use-package ob-ipython
+  :ensure t)
+
 (use-package org
-    :mode (("\\.org$" . org-mode))
-    :ensure t
-    :bind (("\C-cl" . org-store-link)
-           ("\C-ca" . org-agenda)
-           ("\C-cb" . org-iswitchb)
-           ("\C-cc" . org-capture))
-    :config
-    (require 'org-habit)
+  :mode (("\\.org$" . org-mode))
+  :ensure t
+  :bind (("\C-cl" . org-store-link)
+         ("\C-ca" . org-agenda)
+         ("\C-cb" . org-iswitchb)
+         ("\C-cc" . org-capture))
+  :config
+  (require 'org-habit)
   
-    ;; -- ORG PIM --
-    ;;(setq org-agenda-files (list "~/Git/Notes/agenda.org"))
-    (setq org-default-notes-file "~/Documents/Notes/Daily.org")
+  ;; -- ORG PIM --
+  ;;(setq org-agenda-files (list "~/Git/Notes/agenda.org"))
+  (setq org-default-notes-file "~/Documents/Notes/Daily.org")
   
-    (setq org-agenda-files (quote ("~/Documents/Notes/")))
-    (setq org-todo-keywords
-          '((sequence "TODO(t@)" "IN PROGRESS(i@)" "|" "DONE(d@/!)")
-            (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f!)")
-            (sequence "|" "CANCELED(c@/!)")
-            (sequence "|" "WAITING(w@/!)")
-            ))
+  (setq org-agenda-files (quote ("~/Documents/Notes/")))
+  (setq org-todo-keywords
+        '((sequence "TODO(t@)" "IN PROGRESS(i@)" "|" "DONE(d@/!)")
+          (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f!)")
+          (sequence "|" "CANCELED(c@/!)")
+          (sequence "|" "WAITING(w@/!)")
+          ))
   
-    ;; -- Sync Org Pim --
-    (setq org-icalendar-include-todo t)     ;; Exports org-files with TODO items. 
+  ;; -- Sync Org Pim --
+  (setq org-icalendar-include-todo t)     ;; Exports org-files with TODO items. 
   
-    ;; -- Org Programming --
-    (setq org-src-fontify-natively t)
-    (add-hook 'text-mode-hook '(lambda () (auto-fill-mode 1)))
-    (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+  ;; -- Org Programming --
+  (setq org-src-fontify-natively t)
+  (add-hook 'text-mode-hook '(lambda () (auto-fill-mode 1)))
+  (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
   
-    ;; -- Babelfish --
-    (org-babel-do-load-languages
-     'org-babel-load-languages
-     '(
-       (ditaa      . t)
-       (emacs-lisp . t)
-       (latex      . t)
-       (python     . t)
-       (R          . t)
-       ;;(sh         . t)
-       (sql        . t)
-       (sqlite     . t)
-       ))
+  ;; -- Babelfish --
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '(
+     (ditaa      . t)
+     (emacs-lisp . t)
+     (ipython    . t)
+     (latex      . t)
+     (python     . t)
+     (R          . t)
+     ;;(sh         . t)
+     (sql        . t)
+     (sqlite     . t)
+     ))
   
-    ;; Disables org-mode from asking for permission to run stuff
-    ;; Make sure you know what you are doing if you use this.
-    (setq org-confirm-babel-evaluate nil)
+  ;; Disables org-mode from asking for permission to run stuff
+  ;; Make sure you know what you are doing if you use this.
+  (setq org-confirm-babel-evaluate nil)
   
-    ;; Use HTML5 Export --
-    (setq org-html-doctype "html5")
+  ;; Use HTML5 Export --
+  (setq org-html-doctype "html5")
   
-    ;; -- Inline graphics --
-    (setq org-display-inline-images t)
-    (setq org-redisplay-inline-images t)
-    (setq org-startup-with-inline-images "inlineimages")
-    ;;(add-hook 'org-babel-after-execute-hook 'org-display-inline-images)   
-    ;;(add-hook 'org-mode-hook 'org-display-inline-images)
-    )
+  ;; -- Inline graphics --
+  (setq org-display-inline-images t)
+  (setq org-redisplay-inline-images t)
+  (setq org-startup-with-inline-images t)
+  ;;(add-hook 'org-babel-after-execute-hook 'org-display-inline-images)   
+  ;;(add-hook 'org-mode-hook 'org-display-inline-images)
+  )
