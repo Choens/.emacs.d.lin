a4961ff00cb1af43a3a9d3bbc131c25ede0ef755
Andy Choens <andy.choens@acuitashealth.com>
2019-01-03 14:31:45 -0500

    Initial effort at defining a poly-pweave mode for pweave.

--- lisp/polymode-config.el
+++ lisp/polymode-config.el
@@ -4,10 +4,26 @@
 ;; - https://github.com/polymode/polymode
 
 (use-package poly-markdown
-    :ensure t
-    :mode (("\\.mdw"   . poly-markdown-mode)
-           ("\\.Rmd"   . poly-markdown+r-mode)
-           ("\\.Snw"   . poly-noweb+r-mode)
-           ("\\.Rnw"   . poly-noweb+r-mode)
-           ("\\.Rpres" . poly-noweb+r-mode)
-           ))
+  :ensure t
+  :mode (("\\.mdw"   . poly-markdown-mode)
+         ("\\.Pmd"   . poly-pweave-mode)
+         ("\\.Rmd"   . poly-markdown+r-mode)
+         ("\\.Rpres" . poly-noweb+r-mode)
+         ("\\.Rnw"   . poly-noweb+r-mode)
+         ("\\.Snw"   . poly-noweb+r-mode)
+         )
+  :config
+  ;; define pwn polymode
+  (require 'poly-noweb)
+  (require 'poly-markdown)
+
+  (defcustom pm-inner/noweb-python
+    (clone pm-inner/noweb
+           :name "noweb-python"
+           :mode 'python-mode)
+    "Noweb for Python"
+    :group 'poly-innermodes
+    :type 'object)
+
+  (define-polymode poly-pweave-mode poly-markdown-mode
+    :innermodes '(pm-inner/noweb-python :inherit)))
